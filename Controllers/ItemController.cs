0ddb61ff19700cb4caadbbc095cf0d43cf82feb1
Tresor Tshimanga <tshims79@gmail.com>
2022-02-16 23:03:30 +0200

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using IdaApp.Data;
+using IdaApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdaApp.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdaApp.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Borrower { get; set; }
+
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
+
+    }
+}
